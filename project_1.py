1a78414695479d74fbe92cbae6b9644b8241c362
Benny James Clores <cloresbennyjames25@gmail.com>
2023-11-21 20:07:27 +0800

    task_1

---
+++ project_1.py
@@ -0,0 +1,8 @@
+print ("SELF INTRODUCTION GENERATOR")
+
+name = input("What is your name? ")
+dream = input("What is your dream in life? ")
+
+print("Hi, my name is " + name + ".")
+print("I want to be a " + dream + ".")
+
